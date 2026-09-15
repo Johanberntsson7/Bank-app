97400bb0d41448fc3a830c6d264499ff719b2b44
johan-berntsson7@hotmail.com <johan-berntsson7@hotmail.com>
2026-09-15 11:46:08 +0200

    Följde inspelningen

--- BankClass.cs
+++ BankClass.cs
@@ -2,6 +2,23 @@ namespace Bank
 {
     public class BankClass
     {
-        // Här kan du lägga till egenskaper och metoder för BankClass
+        // Klass är uppbyggt av Attribut och Metoder 
+
+        // Dessa är attributer av klass Bank
+        public string BankName;
+
+        // Andra attribut
+        public string OrganisationNumber;
+
+        // Metoderna som bank klassen kan göra
+        public void TransferMoney()
+        {
+            Console.WriteLine("Transfering money");
+        }
+
+        public void Login ()
+        {
+            Console.WriteLine("Trying to login");
+        }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+
+            // Här vill jag skapa ett objekt av klass Bank
+            BankClass bank1 = new BankClass();
+
+
+            bank1.OrganisationNumber = "123456789";
+            bank1.BankName = "nordea";
+
+            
+
+            // Här kallar jag på bank1 metod som är transfer
+            bank1.TransferMoney();
+
+            // HÄR SKA MAN LÄGGA TILL SAKER
+        }
+
+    }
+}
